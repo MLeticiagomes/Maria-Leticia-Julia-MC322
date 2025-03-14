41df6f32b2c557aee008f6aa259eef401a4c9356
MLeticiagomes <m281318@dac.unicamp.br>
2025-03-14 10:46:53 -0300

    versão final

--- Lab01/Main.java
+++ Lab01/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        Robo robo = new Robo("Renaldo", 13, 4);
-        Ambiente ambiente = new Ambiente(20, 20);
+        Robo robo = new Robo("Robo1", 13, 4); /* nome do robo, posição x e posicção y  e criação de um novo robo*/
+        Ambiente ambiente = new Ambiente(20, 20); /* altura e largura do ambiente e criação de um novo robo*/
         
         
-        int coordenada_x = robo.setCoordenadas_x(8);
-        int coordenada_y = robo.setCoordenadas_y(7);;
-        if(ambiente.dentroDosLimites(coordenada_x, coordenada_y) == true);{
+        int coordenada_x = robo.setCoordenadas_x(2); /* altera a posiçõa do robo em x */
+        int coordenada_y = robo.setCoordenadas_y(2); /* altera a posição do robo em y */
+        if(ambiente.dentroDosLimites(coordenada_x, coordenada_y) == true);{ /* verifica se o robo esta dentro dos limites */
             robo.printar();
         }
 
--- Lab01/Robo.java
+++ Lab01/Robo.java
@@ -4,26 +4,26 @@ public class Robo {
     private int coordenada_x;
     private int coordenada_y; 
 
-    public Robo(String nome, int x, int y){
+    public Robo(String nome, int x, int y){ /* cria um robo com nome e posição x e y */
         this.nome_do_robo = nome;
         this.coordenada_x = x;
         this.coordenada_y = y;
 
     }
 
-    public int setCoordenadas_x(int deltax){
+    public int setCoordenadas_x(int deltax){ /* muda a coordenada x */
         this.coordenada_x += deltax;
         return coordenada_x;
         
     }
 
-    public int setCoordenadas_y(int deltay){
+    public int setCoordenadas_y(int deltay){ /* muda a coordenada y */
         this.coordenada_y += deltay;
         return coordenada_y;
         
     }
 
-    public void printar(){
+    public void printar(){ /* printa as coordenadas */
     System.out.println("Nova posicao : (" + coordenada_x + "," + coordenada_y+ ")");
     }
 
