7a699c1ebf0f5562ca1b0136a3a988b81cb4ed5a
Julia <j281272@dac.unicamp.br>
2025-03-20 13:58:18 -0300

    Algumas correcoes na main

--- Lab01/Main.java
+++ Lab01/Main.java
@@ -7,41 +7,45 @@ public class Main {
         int largura = scanner.nextInt();
         int altura = scanner.nextInt();
         Ambiente ambiente = new Ambiente(largura, altura); /* altura e largura do ambiente e criação de um novo robo*/
+        Robo robo= null;
             
         while (true){
             System.out.println("O que deseja fazer?\nDigite 1 para criar novo robo\nDigite 2 para mover o robo\nDigite 0 para fechar o simulador");
-            int comando=scanner.next();
+            int comando=scanner.nextInt();
             if (comando==1){
                 System.out.println("Digite o nome, coordenada x, coordenada y e direcao (separadas por espaco):");
                 String nome = scanner.next();
                 int coordenadaX = scanner.nextInt(); 
                 int coordenadaY = scanner.nextInt();
                 String direcao = scanner.next(); 
-                Robo robo = new Robo(nome, coordenadaX, coordenadaY, direcao); /* nome do robo, posição x e posicção y  e criação de um novo robo*/
+                robo = new Robo(nome, coordenadaX, coordenadaY, direcao); /* nome do robo, posição x e posicção y  e criação de um novo robo*/
             }
             else if (comando==2){
+                System.out.println("Digite a distancia percorrida e a direcao (separadas por espaco):");
+                int distancia=scanner.nextInt();
+                String direcao = scanner.next(); 
                 while (true){
-                    System.out.println("Digite a distancia percorrida e a direcao (separadas por espaco):");
-                    int distancia=scanner.nextInt();
-                    string direcao = scanner.next(); 
-                    while (true){
-                        if (direcao.equalsIgnoreCase("norte")|| direcao.equalsIgnoreCase("sul")){
-                            int coordenada_y = robo.setCoordenadas_y(distancia); /* altera a posição do robo em y */
-                            if(ambiente.dentroDosLimites(0, coordenada_y) == true);{ /* verifica se o robo esta dentro dos limites */
-                                robo.printar();
+                    if (direcao.equalsIgnoreCase("norte")|| direcao.equalsIgnoreCase("sul")){
+                        int coordenada_y = robo.setCoordenadas_y(distancia); /* altera a posição do robo em y */
+                        if(ambiente.dentroDosLimites(0, coordenada_y) == true){ /* verifica se o robo esta dentro dos limites */
+                            robo.printar();
                                 break;
-                            }
                         }
-                        else if (direcao.equalsIgnoreCase("oeste")|| direcao.equalsIgnoreCase("leste")){
-                            int coordenada_x = robo.setCoordenadas_x(distancia); /* altera a posiçõa do robo em x */
-                            if(ambiente.dentroDosLimites(coordenada_x, 0) == true);{ /* verifica se o robo esta dentro dos limites */
-                                robo.printar();
-                                break;
-                            }
+                    }
+                    else if (direcao.equalsIgnoreCase("oeste")|| direcao.equalsIgnoreCase("leste")){
+                        int coordenada_x = robo.setCoordenadas_x(distancia); /* altera a posiçõa do robo em x */
+                        if(ambiente.dentroDosLimites(coordenada_x, 0) == true){ /* verifica se o robo esta dentro dos limites */
+                            robo.printar();
+                            break;
                         }
                     }
                 }
             }
+            else if (comando==0){
+                System.out.println("Encerrando o simulador...");
+                scanner.close();
+                break;
+            } 
         }
     }
 }
