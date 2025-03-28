df0c2d221e4fee97d70619a29928e228db8a337c
Julia <j281272@dac.unicamp.br>
2025-03-28 10:50:20 -0300

    Atualizacao RoboAereo

--- Lab02/RoboAereo.java
+++ Lab02/RoboAereo.java
@@ -2,21 +2,15 @@ public class RoboAereo extends Robo{
 
     private int altitude;
     private int altitudeMaxima;
-    private Ambiente ambiente;
-    private Robo robo;
 
-
-    public RoboAereo(String nome, int x, int y, String d, int alt, int alt_max){
+    public RoboAereo(String nome, int x, int y, String d, int alt_max){
         super(nome,x,y,d);
-        this.altitude = alt;
-        this.altitudeMaxima = alt_max;
-        this.ambiente = ambiente;
-        this.robo = robo; 
-        
+        this.altitude = y;
+        this.altitudeMaxima = alt_max;  
     }
 
     public int getaltura_maxima(){ /* pega a altura maxima como a do ambiente */
-       return ambiente.getAltura();
+       return altitudeMaxima;
     }
 
     public int getaltitude(){ /* pega a altitude do robo como sua coordenada y */
@@ -32,9 +26,7 @@ public class RoboAereo extends Robo{
 
         else{
             System.out.println("Altitude acima do permitido :(");
-        }
-
-        
+        }    
     }
 
     public void descer(int altitude,int deltay, int d){
